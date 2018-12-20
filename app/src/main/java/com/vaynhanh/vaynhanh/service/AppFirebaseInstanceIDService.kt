69d28b2a4eb73930baa5b75ccb4dc36dd0470d0b
administrator <dongzhongyang@lanlingdai.net>
2018-12-20 18:06:09 +0800

    update:FCM推送部分完成

--- app/src/main/java/com/vaynhanh/vaynhanh/service/AppFirebaseInstanceIDService.kt
+++ app/src/main/java/com/vaynhanh/vaynhanh/service/AppFirebaseInstanceIDService.kt
@@ -1,9 +1,10 @@
 package com.vaynhanh.vaynhanh.service
 
+import android.content.Intent
 import android.util.Log
-import com.google.firebase.iid.FirebaseInstanceIdService
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.vaynhanh.vaynhanh.MainActivity
 
 class AppFirebaseInstanceIDService : FirebaseMessagingService(){
     override fun onNewToken(p0: String?) {
@@ -12,6 +13,9 @@ class AppFirebaseInstanceIDService : FirebaseMessagingService(){
     }
 
     override fun onMessageReceived(p0: RemoteMessage?) {
+        Log.d("TAG","${p0!!.messageType}")
+        var intent = Intent(this@AppFirebaseInstanceIDService,MainActivity::class.java )
+        startActivity(intent)
         super.onMessageReceived(p0)
     }
 }
